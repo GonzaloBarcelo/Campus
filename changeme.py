692b7728c3953ec4b8c203f9bf4cb33f5aabdeef
GonzaloBarcelo <78573947+GonzaloBarcelo@users.noreply.github.com>
2023-07-10 12:53:23 +0200

    Create changeme.py

---
+++ changeme.py
@@ -0,0 +1,16 @@
+nombre = int(input("dime tu nombre:"))
+apellido = int(input("dime tu apellido:"))
+
+print("Opciones: 1. Sacar dinero 2. Ingresar dinero 3. Ver balance 4. Cerrar")
+input("¿Que opción desea ejecutar?")
+if 1:
+    input("¿Cuanto dinero desea retirar?")
+    dinero = dinero_a_retirar
+if 2:
+    input("¿Cuanto dinero desea ingresar?")
+    dinero = dinero_a_ingresar
+
+if 3:
+    input(balance)
+if 4:
+    cerrar()
